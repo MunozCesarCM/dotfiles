cbb02bed4e3ee43f5bcbbfc48f137f7175263427
MunozCesarCM <munozcesar.cm@gmail.com>
2023-03-31 20:31:04 -0600

    Refactor

--- scripts/change-wallpaper.sh
+++ scripts/change-wallpaper.sh
@@ -1,6 +1,25 @@
 #!/bin/sh
 
-cd ~/images/wallpaper
-file=$(find -type f | cut -c 3- | fzf)
-sed -i "s/wallpaper\/.*--/wallpaper\/$file --/" ~/.config/X11/xinitrc
-feh --bg-fill ~/images/wallpaper/$file --no-fehbg &
+# Check for dependencies
+if ! command -v feh > /dev/null; then
+  echo "Error: feh is not installed. Aborting."
+  exit 1
+fi
+
+# Set directories
+WALLPAPER_DIR="$HOME/images/wallpaper"
+XINIT_DIR="$HOME/.config/X11/xinitrc"
+
+# Check if wallpaper directory is empty
+if [ -z "$(ls -A $WALLPAPER_DIR)" ]; then
+  echo "Error: $WALLPAPER_DIR is empty. Aborting."
+  exit 1
+fi
+
+# Select an image from list
+SELECTED_IMAGE=$(ls $WALLPAPER_DIR | grep -E '\.(png|jpg|jpeg)$' | fzf)
+SELECTED_IMAGE="$WALLPAPER_DIR/$SELECTED_IMAGE"
+
+# Set the wallpaper
+feh --bg-fill "$SELECTED_IMAGE"
+sed  -i "s|feh.*|feh --bg-fill $SELECTED_IMAGE --no-fehbg \&|" "$XINIT_DIR"
