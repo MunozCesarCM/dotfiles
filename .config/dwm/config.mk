4400ad17dd4b8e326961a7f2ef93329cd0cee41f
MunozCesarCM <munozcesar.cm@gmail.com>
2023-04-26 21:19:30 -0600

    Changed config for Void Linux

--- .config/dwm/config.mk
+++ .config/dwm/config.mk
@@ -7,8 +7,12 @@ VERSION = 6.3
 PREFIX = /usr/local
 MANPREFIX = ${PREFIX}/share/man
 
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
+X11INC = /usr/include/X11
+X11LIB = /usr/lib/X11
+
+# Arch Linux (uncomment)
+#X11INC = /usr/X11R6/include
+#X11LIB = /usr/X11R6/lib
 
 # Xinerama, comment if you don't want it
 XINERAMALIBS  = -lXinerama
