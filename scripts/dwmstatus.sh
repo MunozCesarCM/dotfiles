0088e60a795c3fef82185edc44ab1fd202e59445
MunozCesarCM <munozcesar.cm@gmail.com>
2023-05-14 21:17:44 -0600

    Removed unused code

--- scripts/dwmstatus.sh
+++ scripts/dwmstatus.sh
@@ -4,41 +4,29 @@ while true; do
     # Initial padding
     statusbar=" "
 
-    # CPU
-    #CPU=$(top -bn1 | grep Cpu | awk '{print $2}')%
-    #statusbar="$statusbar$(printf "CPU %s | " "$CPU")"
-
     # RAM used
     MEM=$(free -h | grep Mem)
     MEMUSED=$(echo $MEM | awk '{print $3}')
-    #MEMUTOT=$(echo $MEM | awk '{print $2}')
-    statusbar="$statusbar$(printf " %s | " "$MEMUSED")"
+    statusbar="$statusbar$(printf "%s | " "$MEMUSED")"
 
     # Battery
     BAT=$(acpi | awk '{print $4}' | sed 's/.$//')
     DIS=$(acpi | awk '{print $3}')
     if [ "$DIS" != "Discharging," ]; then
-        statusbar="$statusbar$(printf " [%s] | " "$BAT")"
+        statusbar="$statusbar$(printf "[%s] | " "$BAT")"
     else
-        statusbar="$statusbar$(printf " %s | " "$BAT")"
+        statusbar="$statusbar$(printf "%s | " "$BAT")"
     fi
 
     # Internet connection
     ONLINE=$(ip a | grep DOWN | wc -l)
     if [ "$ONLINE" != "0" ]; then
-        statusbar="$statusbar$(printf "睊 | ")"
+        statusbar="$statusbar$(printf "▽ | ")"
     else
-        statusbar="$statusbar$(printf " | ")"
+        statusbar="$statusbar$(printf "▲ | ")"
     fi
 
-    # Show tasks to be completed if amount > 0
-    TASK=$(cat ~/documents/text/todo | grep "^*" | wc -l | awk '{print $1}')
-    if [ "$TASK" != "0" ]; then
-        statusbar="$statusbar$(printf " %s | " "$TASK")"
-    fi
-
-    # Date and time
-    # statusbar="$statusbar$(printf "%s" "$(date "+%I:%M:%S ")")"
+    # Time
     statusbar="$statusbar$(printf "%s" "$(date "+%R ")")"
 
     # End padding
