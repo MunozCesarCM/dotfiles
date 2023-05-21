0b88c6ecf20febcff99fffb182bd56e955db7b05
MunozCesarCM <munozcesar.cm@gmail.com>
2023-05-21 11:53:07 -0600

    Changed script to open non terminal files

--- scripts/fuzzy-find-file.sh
+++ scripts/fuzzy-find-file.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 # Go to home directory and search for files
-cd
-file=$(fd -H \
+file=$(fd . ~ -H \
   -E .cache \
   -E .vim \
   -E .pki \
@@ -16,40 +15,41 @@ file=$(fd -H \
   --type f | fzf )
 
 # Get file extension
-extension=$(echo ${file} | awk -F . '{print $NF}')
-extension=$(echo ${extension} | awk '{print tolower($0)}')
+extension=${file##*.}
+extension=${extension,,}
 
 # Get file directory and cd into it
-# This is so coc can work correctly
-absolutedir=$(echo $file | sed 's%/[^/]*$%/%')
+absolutedir=${file%/*}/
 cd "$absolutedir"
 
 # Strip filename
-file=$(echo $file | sed "s/.*\///")
+file=${file##*/}
 
 # Dont open anything if the user quit
 if [ "$file" != '' ]; then
-    case $extension in
-        mp3 ) mpv --no-video "$file" ;;
-        wav ) mpv --no-video "$file" ;;
-        wma ) mpv --no-video "$file" ;;
-        m4a ) mpv --no-video "$file" ;;
-        flac) mpv --no-video "$file" ;;
+  case $extension in
+    mp3 ) mpv --no-video "$file" ;;
+    wav ) mpv --no-video "$file" ;;
+    wma ) mpv --no-video "$file" ;;
+    m4a ) mpv --no-video "$file" ;;
+    flac) mpv --no-video "$file" ;;
 
-        mp4 ) devour mpv "$file" ;;
-        mov ) devour mpv "$file" ;;
-        wmv ) devour mpv "$file" ;;
-        mkv ) devour mpv "$file" ;;
+    mp4 ) nohup mpv "$file" >/dev/null 2>&1 & ;;
+    mov ) nohup mpv "$file" >/dev/null 2>&1 & ;;
+    wmv ) nohup mpv "$file" >/dev/null 2>&1 & ;;
+    mkv ) nohup mpv "$file" >/dev/null 2>&1 & ;;
 
-        jpg ) devour feh "$file" ;;
-        jpeg) devour feh "$file" ;;
-        png ) devour feh "$file" ;;
-        webp) devour feh "$file" ;;
-        gif ) devour feh "$file" ;;
+    jpg ) feh "$file" ;;
+    jpeg) feh "$file" ;;
+    png ) feh "$file" ;;
+    webp) feh "$file" ;;
+    gif ) feh "$file" ;;
 
-        pdf ) devour zathura "$file" ;;
-        epub) devour zathura "$file" ;;
+    pdf ) nohup zathura "$file" >/dev/null 2>&1 & ;;
+    epub) nohup zathura "$file" >/dev/null 2>&1 & ;;
 
-        *) nvim "$file"
-    esac
+    *) nvim "$file"
+  esac
+
+  sleep 0.5
 fi
