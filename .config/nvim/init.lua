b12c9eb4bec3897a0efd428cb55f3a935a3b29da
MunozCesarCM <munozcesar.cm@gmail.com>
2024-01-01 20:01:14 -0600

    Integrated prettier

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -231,6 +231,32 @@ require('lazy').setup({
     end
   },
 
+  -- Prettier
+  { 'jose-elias-alvarez/null-ls.nvim' },
+  {
+    'MunifTanjim/prettier.nvim',
+    config = function()
+      local prettier = require("prettier")
+      prettier.setup({
+        bin = 'prettier', -- or `'prettierd'` (v0.23.3+)
+        filetypes = {
+          "css",
+          "graphql",
+          "html",
+          "javascript",
+          "javascriptreact",
+          "json",
+          "less",
+          "markdown",
+          "scss",
+          "typescript",
+          "typescriptreact",
+          "yaml",
+        },
+      })
+    end,
+  },
+
   -- LSP Support
   {'williamboman/mason.nvim'},
   {'williamboman/mason-lspconfig.nvim'},
